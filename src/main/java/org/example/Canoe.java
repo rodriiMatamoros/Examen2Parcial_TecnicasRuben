a0d5c79e43fe5c166a8fb0bd44580cc35366e4e4
Rodrigo Matamoros Becerra <rodri@MacBook-Pro-de-Rodrigo.local>
2023-04-13 17:03:24 +0200

    Commit Canoe

---
+++ src/main/java/org/example/Canoe.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Canoe extends Ship {
+    private static final int SIZE = 1;
+
+    public Canoe(int x, int y, CardinalPoints direction) {
+        super(1, x, y, direction);
+    }
+}
+
