325163e04ddbbecafc9d49a950ea7908d4c73545
Rodrigo Matamoros Becerra <rodri@MacBook-Pro-de-Rodrigo.local>
2023-04-13 17:02:25 +0200

    Commit Frigate

---
+++ src/main/java/org/example/Frigate.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Frigate extends Ship {
+    private static final int SIZE = 3;
+
+    public Frigate(int x, int y, CardinalPoints direction) {
+        super(3, x, y, direction);
+    }
+}
+
